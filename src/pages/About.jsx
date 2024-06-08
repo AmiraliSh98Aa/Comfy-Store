b50ef8188ffbacc67bdc359865cf1364d07e088e
Amirali Shahabi <shahabi.amiirali@gmail.com>
2024-06-08 02:26:08 -0700

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,24 @@
+
+const About = () => {
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-light sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore ipsum molestiae, illo cupiditate distinctio, soluta consequatur mollitia provident, blanditiis dolorem dolor delectus suscipit itaque placeat animi. Vel recusandae maxime quis suscipit, dolore autem assumenda, velit nihil minima incidunt voluptate fuga?
+      </p>
+    </>
+  )
+}
+
+export default About
